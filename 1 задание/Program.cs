27e4118d27c29dee5be90d26bd23d14af52dfd30
Angelina-te <113330744+Angelina-te@users.noreply.github.com>
2022-10-19 02:15:47 +0700

    3

---
+++ 1 задание/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: "); string number = Console.ReadLine(); 
+int length = number.Length;
+
+if (length == 5)
+{
+if (number[0] == number[4] && number[1] == number[3])    
+// 0 1 2 3 4
+//[1 1 2 1 2]
+{
+    Console.WriteLine($"{number} - Число является палиндромом");
+}
+else
+{
+    Console.WriteLine($"{number} - Чиисло не является палиндромом");
+}
+}
+
